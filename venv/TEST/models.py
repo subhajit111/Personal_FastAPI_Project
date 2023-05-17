77b10e81ae4abbccbbbb1b88ba453ddb2cd984d8
subhajit111 <mishra.subhajit1998@gmail.com>
2023-05-17 14:04:44 +0530

    "done"

--- venv/TEST/models.py
+++ venv/TEST/models.py
@@ -1,3 +1,4 @@
+from pydoc import text
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, MetaData
 from TEST.database import Base
 
@@ -6,5 +7,5 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
+    create_at = Column(TIMESTAMP(timezone=True), nullable= False, server_default= text('now()'))
